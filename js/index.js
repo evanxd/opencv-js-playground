e8e2c8962b74f8b725514f3adca9484599826d49
Tseng-jen (Evan) Tseng <evan@tseng.io>
2021-02-02 14:07:15 +0800

    Make a page to do image edge detection

---
+++ js/index.js
@@ -0,0 +1,23 @@
+(() => {
+  const fileUploader = document.querySelector('#file-uploader')
+  const image = document.querySelector('#image')
+
+  fileUploader.addEventListener('change', e => {
+    image.src = URL.createObjectURL(e.target.files[0])
+  }, false)
+
+  image.onload = () => {
+    const input = cv.imread(image)
+    const result = new cv.Mat()
+    cv.cvtColor(input, input, cv.COLOR_RGB2GRAY, 0)
+    cv.Canny(input, result, 70, 180, 3, false)
+    cv.imshow('canvas', result)
+    result.delete()
+    input.delete()
+  }
+
+  window.onOpenCvReady = () => {
+    document.querySelector('#message').remove()
+    fileUploader.removeAttribute('disabled')
+  }
+})()
